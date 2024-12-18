7cd399d3285f988a617beac759fd830b3cc5a9ce
Nezar-Gazer-ENG <nethird703@gmail.com>
2024-12-18 17:55:39 +0200

    Initial commit: Java project

---
+++ api20216146/src/main/java/com/sdaproject/api20216146/Api20216146Application.java
@@ -0,0 +1,13 @@
+package com.sdaproject.api20216146;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api20216146Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Api20216146Application.class, args);
+	}
+
+}
---
+++ api20216146/src/test/java/com/sdaproject/api20216146/Api20216146ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdaproject.api20216146;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Api20216146ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
